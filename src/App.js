6e5d349bbb455cce79c1889ab296a4f78e652353
Rasmus Vind <noreply@vindit.dk>
2019-12-03 09:20:53 +0100

    Replace moment with date-fns.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
-import moment from 'moment';
+import { isAfter, format, isBefore } from 'date-fns';
 
 class App extends React.Component {
   constructor()
   {
     super();
     this.state = this.getNextState();
+    this.state.switch = false;
   }
 
   componentDidMount() {
@@ -18,22 +19,61 @@ class App extends React.Component {
   }
 
   getNextState() {
-    const morning = moment().hours(6).minutes(15);
-    const evening = moment().hours(18).minutes(45);
-    // const evening = moment().hours(10);
-    const mustSleep = !moment().isBetween(morning, evening, '[]');
-    const time = moment().format('HH:mm');
+    const morning = new Date()
+    morning.setSeconds(0)
+    const testing = false;
+    if (testing)
+    {
+      morning.setHours(9)
+      morning.setMinutes(12)
+    }
+    else
+    {
+      morning.setHours(6)
+      morning.setMinutes(15)
+    }
+    const evening = new Date()
+    evening.setSeconds(0)
+    if (testing)
+    {
+      evening.setHours(9)
+      evening.setMinutes(12)
+      evening.setSeconds(10)
+    }
+    else
+    {
+      evening.setHours(18)
+      evening.setMinutes(45)
+    }
+    const now = new Date()
+    const mustSleep = !(isAfter(now, morning) && isBefore(now, evening));
+    const time = format(now, 'HH:mm');
     return { mustSleep, time };
   }
 
+  switch()
+  {
+    this.setState({switch: !this.state.switch});
+  }
+
+  mustSleep()
+  {
+    return this.state.switch ? !this.state.mustSleep : this.state.mustSleep;
+  }
+
   checkSleep() {
-    this.setState(this.getNextState());
+    console.log(JSON.stringify(this.state, null, 2));
+    const nextState = this.getNextState();
+    if (nextState.mustSleep !== this.state.mustSleep)
+    {
+      nextState.switch = false;
+    }
+    this.setState(nextState);
   }
 
   render() {
     return (
-      <div className={"App " + (this.state.mustSleep ? 'sleep' : 'wake')}>
-        {/* <div className="show"></div> */}
+      <div className={"App " + (this.mustSleep() ? 'sleep' : 'wake')} onClick={this.switch.bind(this)}>
         <div className="bottom">
           <div className="time">
             {this.state.time}
